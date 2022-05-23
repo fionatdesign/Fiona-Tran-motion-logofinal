71b3d1dbcd1bd1ffa14396dfc0a2ef0a501b4edf
fionatdesign <ft98@drexel.edu>
2022-05-23 14:10:21 -0400

    imported modules

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,18 @@
 import { gsap } from "gsap";
+
+import { CustomEase } from "gsap/CustomEase";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+import { GSDevTools } from "gsap/GSDevTools";
+import { MotionPathHelper } from "gsap/MotionPathHelper";
+import { CustomBounce } from "gsap/CustomBounce";
+import { CustomWiggle } from "gsap/CustomWiggle";
+
+gsap.registerPlugin(GSDevTools);
+gsap.registerPlugin(DrawSVGPlugin, MorphSVGPlugin);
+gsap.registerPlugin(CustomEase, MotionPathHelper, CustomBounce, CustomWiggle);
+// MorphSVGPlugin.convertToPath("#circle, .dot");
+
+
+const mainTL = gsap.timeline({id: "mainTL"});
+
