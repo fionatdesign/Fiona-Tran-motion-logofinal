d0b3ee5fd24efdf0e4f212f77a264e63b2934859
fionatdesign <ft98@drexel.edu>
2022-05-29 18:38:10 -0400

    work on cherry branch animation

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -11,8 +11,25 @@ import { CustomWiggle } from "gsap/CustomWiggle";
 gsap.registerPlugin(GSDevTools);
 gsap.registerPlugin(DrawSVGPlugin, MorphSVGPlugin);
 gsap.registerPlugin(CustomEase, MotionPathHelper, CustomBounce, CustomWiggle);
-// MorphSVGPlugin.convertToPath("#circle, .dot");
+MorphSVGPlugin.convertToPath("#branch");
 
 
 const mainTL = gsap.timeline({id: "mainTL"});
 
+
+function cherryBranch(){
+    let tl = gsap.timeline();
+
+    tl.from("#branch", {y: -30, duration: 2, drawSVG: 0}, "animate")
+    .from("#leaf", {display: "none", scale: .2, duration:2, drawSVG: 0, ease: "expo.out"}, "animate+=1.3")
+    .from("#Vector_44", {display: "none", scale: .2, duration:2, ease: "expo.out"}, "animate+=1.3")
+    .from("#cherrybody1", {display: "none", scale: .2, duration:2, transformOrigin: "top", ease: "expo.out"}, "animate+=1.8")
+
+    return tl;
+    
+}
+
+mainTL
+.add(cherryBranch())
+
+;
