110aa1db78bfb4dc98a0c8e3737e48b82193a469
fionatdesign <ft98@drexel.edu>
2022-05-29 20:38:15 -0400

    added bounce to cherry

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -11,7 +11,7 @@ import { CustomWiggle } from "gsap/CustomWiggle";
 gsap.registerPlugin(GSDevTools);
 gsap.registerPlugin(DrawSVGPlugin, MorphSVGPlugin);
 gsap.registerPlugin(CustomEase, MotionPathHelper, CustomBounce, CustomWiggle);
-MorphSVGPlugin.convertToPath("#branch");
+MorphSVGPlugin.convertToPath("#branch", "#leaf", "cherrybody1", "cherrybody2");
 
 
 const mainTL = gsap.timeline({id: "mainTL"});
@@ -21,9 +21,17 @@ function cherryBranch(){
     let tl = gsap.timeline();
 
     tl.from("#branch", {y: -30, duration: 2, drawSVG: 0}, "animate")
-    .from("#leaf", {display: "none", scale: .2, duration:2, drawSVG: 0, ease: "expo.out"}, "animate+=1.3")
-    .from("#Vector_44", {display: "none", scale: .2, duration:2, ease: "expo.out"}, "animate+=1.3")
-    .from("#cherrybody1", {display: "none", scale: .2, duration:2, transformOrigin: "top", ease: "expo.out"}, "animate+=1.8")
+    .from("#leaf", {display: "none", scale: .2, duration:2, ease: "expo.out"}, "animate+=1.3")
+    .from("#cherrybranch", {display: "none", scale: .1, duration:2, drawSVG:0, transformOrigin: "top right", ease: "expo.out"}, "animate+=1.8")
+    .from("#leaf", {duration:1, yoyo: true, rotate: 5, repeat: 10}, "animate+=1.3")
+    .from("#cherrybranch", {duration:1, yoyo: true, rotate: 5, repeat: 6}, "animate+=1.3")
+    .to("#branch", {duration:1, y:-400}, "animate+=8")
+    .to("#leaf", {duration:1, y:-500}, "animate+=8")
+    .to("#cherrybranch", {duration:1, y:125, rotate: 25, transformOrigin: "bottom center", ease: "bounce.out"}, "animate+=8")
+    .to("#cherrybranch", {duration:1, rotate: -100, transformOrigin: "bottom center", ease: "back.out(1.8)"}, "animate+=8.5")
+    .to("#cherrybranch", {duration:1, rotate: 0, transformOrigin: "bottom center", ease: "back.out(1.8)"}, "animate+=8.5")
+    // .to("#cerise-logo", {visibility: "visible"})
+    // .to("#cherrystem", {duration:1, morphSVG: "#stem"})
 
     return tl;
     
