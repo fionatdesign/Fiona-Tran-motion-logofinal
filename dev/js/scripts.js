aab9fa09ce3d07647c75fe0bf39022a8d0f71390
fionatdesign <ft98@drexel.edu>
2022-06-03 10:32:04 -0400

    adjusted timing of arm and leg animation

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -4,12 +4,8 @@ import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
 import { GSDevTools } from "gsap/GSDevTools";
 import { MotionPathPlugin } from "gsap/MotionPathPlugin";
-import { CustomBounce } from "gsap/CustomBounce";
-import { CustomWiggle } from "gsap/CustomWiggle";
 
-gsap.registerPlugin(GSDevTools);
-gsap.registerPlugin(DrawSVGPlugin, MorphSVGPlugin);
-gsap.registerPlugin(MotionPathPlugin, CustomBounce, CustomWiggle);
+gsap.registerPlugin(DrawSVGPlugin, MorphSVGPlugin, GSDevTools, MotionPathPlugin);
 MorphSVGPlugin.convertToPath("#branch", "#leaf", "cherrybody1", "cherrybody2", "#leftleg", "#rightleg", "#cherisename", "#icecreamname");
 
 
@@ -19,18 +15,18 @@ const mainTL = gsap.timeline({id: "mainTL"});
 function cherryBranch(){
     let tl = gsap.timeline();
 
-    tl.from("#branch", {y: -30, duration: 1.5, fill: "#FCF3E3", drawSVG: 0}, "animate")
-    .to("#branch", {duration:1, fill:"#fff"}, "animate+=2")
-    .from("#leaf", {display: "none", scale: .2, duration:2, ease: "expo.out"}, "animate+=1.3")
-    .from("#cherrybranch", {display: "none", scale: .1, duration:2, drawSVG:0, transformOrigin: "130px 10px", ease: "expo.out"}, "animate+=1.8")
-    .from("#leaf", {duration:1, yoyo: true, rotate: 5, repeat: 3}, "animate+=1.3")
-    .from("#cherrybranch", {duration:1, yoyo: true, transformOrigin: "130px 10px", rotate: 5, repeat: 3}, "animate+=1.3")
-    .to("#branch", {duration:1, y:-400}, "animate+=4.5")
-    .to("#leaf", {duration:1, y:-500}, "animate+=4.5")
-    .to("#cherrybranch", {duration:1, y:200, rotate: 25, transformOrigin: "bottom center", ease: "bounce.out"}, "animate+=4.5")
-    .to("#cherrybranch", {duration:1, rotate: -100, transformOrigin: "bottom center", ease: "back.out(1.8)"}, "animate+=5")
-    .to("#cherrybranch", {duration:1, rotate: 0, transformOrigin: "bottom center", x:10, ease: "back.out(3)"}, "animate+=5")
-    .to("#cherrystem", {duration:1, rotate: 30, x:-10, transformOrigin: "bottom", ease: "back.out(3)"}, "animate+=5")
+    tl.from("#branch", {y: -30, duration: 1.5, fill: "#FCF3E3", drawSVG: 0}, "branch")
+    .to("#branch", {duration:1, fill:"#fff"}, "branch+=2")
+    .from("#leaf", {display: "none", scale: .2, duration:2, ease: "expo.out"}, "branch+=1.3")
+    .from("#cherrybranch", {display: "none", scale: .1, duration:2, drawSVG:0, transformOrigin: "130px 10px", ease: "expo.out"}, "branch+=1.8")
+    .from("#leaf", {duration:1, yoyo: true, rotate: 5, repeat: 3}, "branch+=1.3")
+    .from("#cherrybranch", {duration:1, yoyo: true, transformOrigin: "130px 10px", rotate: 5, repeat: 3}, "branch+=1.3")
+    .to("#branch", {duration:1, y:-400}, "branch+=4.5")
+    .to("#leaf", {duration:1, y:-500}, "branch+=4.5")
+    .to("#cherrybranch", {duration:1, y:200, rotate: 25, transformOrigin: "bottom center", ease: "bounce.out"}, "branch+=4.5")
+    .to("#cherrybranch", {duration:1, rotate: -100, transformOrigin: "bottom center", ease: "back.out(1.8)"}, "branch+=5")
+    .to("#cherrybranch", {duration:1, rotate: 0, transformOrigin: "bottom center", x:10, ease: "back.out(3)"}, "branch+=5")
+    .to("#cherrystem", {duration:1, rotate: 30, x:-10, transformOrigin: "bottom", ease: "back.out(3)"}, "branch+=5")
 
     return tl;
     
@@ -102,7 +98,7 @@ function logoName(){
     .from("#apostrophe", {duration: 0.5, drawSVG: 0}, "cherise")
     .from("#second-s", {duration: 0.5, drawSVG: 0}, "cherise")
     .from("#icecreamname", {duration: 0.5, drawSVG: 0})
-    .from(".dot", {opacity: 0, transformOrigin: "center", scale: 2, delay: 1, ease: "power3.out", duration: 0.5})
+    .from(".dot", {opacity: 0, transformOrigin: "center", scale: 2, delay: 0.5, ease: "power3.out", duration: 0.5})
     
 
     return tl;
@@ -111,10 +107,10 @@ function logoName(){
 
 mainTL
 .add(cherryBranch())
-.add(cherryLeg())
-.add(cherryArm())
-.add(cherryFace())
-.add(icecreamMotion())
+.add(cherryLeg(), "bodyparts")
+.add(cherryArm(), "bodyparts+=0.6")
+.add(cherryFace(), "animate")
+.add(icecreamMotion(), "animate+=1")
 .add(logoName())
 
 
